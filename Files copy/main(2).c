5bf7ae8d763e209ec1f38c824aa5c1ef76d00252
Rags56 <Aqubain@outlook.com>
2024-04-04 00:12:28 +0300

    Added test files for showcase
    :) almost done free marks

---
+++ Files copy/main(2).c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+//20231041 alaa qubein
+//q1
+
+//void billcalculator(){
+//int choice;
+//    float hours;
+//    printf("Please Select an option:\n1-Supported\n2-Not Supported\n0-Exit the program\n");
+//    scanf("%d",&choice);
+//    if(choice == 0) {
+//        return 0;
+//    }
+//    else if(choice == 1){
+//        printf("Please enter the amount of Kilowatt spent during one month: \n");
+//        scanf(" %f",&hours);
+//        if (hours < 200) {
+//            printf("Pay %.1f please",hours*0.25);
+//        }
+//        else if (hours >= 200 && hours <= 500){
+//            printf("Pay %.1f please",hours*0.35);
+//        }
+//        else if (hours > 500){
+//            printf("Pay %.1f please",hours*0.60);
+//        }
+//    }
+//    else if(choice == 2){
+//        printf("Please enter the amount of Kilowatt spent during one month: \n");
+//        scanf(" %f",&hours);
+//        if (hours < 500) {
+//            printf("Pay %.1f please",hours*0.30);
+//        }
+//        else if (hours > 500){
+//            printf("Pay %.1f please",hours*0.90);
+//        }
+//    }
+//}
+//int main()
+//{
+//    billcalculator();
+//}
+
+//q2
+//void getMonthDays(month){
+//    switch(month){
+//    case 1:
+//        printf("there are 31 days in month 1");
+//        break;
+//    case 2:
+//        printf("there are 28 days in month 2");
+//        break;
+//    case 3:
+//        printf("there are 31 days in month 3");
+//        break;
+//    case 4:
+//        printf("there are 30 days in month 4");
+//        break;
+//    case 5:
+//        printf("there are 31 days in month 5");
+//        break;
+//    case 6:
+//        printf("there are 30 days in month 6");
+//        break;
+//    case 7:
+//        printf("there are 31 days in month 7");
+//        break;
+//    case 8:
+//        printf("there are 31 days in month 8");
+//        break;
+//    case 9:
+//        printf("there are 30 days in month 9");
+//        break;
+//    case 10:
+//        printf("there are 31 days in month 10");
+//        break;
+//    case 11:
+//        printf("there are 30 days in month 11");
+//        break;
+//    case 12:
+//        printf("there are 31 days in month 12");
+//        break;
+//    default:
+//        printf("Please enter a valid month");
+//    }
+//}
+//
+//void main(){
+//    int month;
+//    printf("Please enter number of month\n");
+//    scanf("%d",&month);
+//    getMonthDays(month);
+//}
--- rename.py
+++ rename.py
@@ -33,6 +33,10 @@ for file_name in os.listdir(folder_path):
             if len(comments_with_digits) > 1:
                 print(f"Check file: {file_name} for multiple student IDs")
                 continue  # Skip renaming if there are multiple IDs
+            #if there no ids also throw an error
+            if len(comments_with_digits) == 0:
+                print(f"Check file: {file_name} no ID could be found")
+                continue
 
         # If there's a single ID, proceed to rename the file
         if comments_with_digits:
