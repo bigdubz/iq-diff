eeea09c7b19a87df8334104145c729d266b22b52
dubz <siencebro@gmail.com>
2024-04-03 19:04:16 +0300

    reformat

--- renaming_script.py
+++ renaming_script.py
@@ -1,7 +1,7 @@
 import os
 import re
 
-folder_path = "Files"
+folder_path = "Files test (COPY TO AVOID RENAMING FILES)"
 
 # Open all main .c files in the folder
 for file_name in os.listdir(folder_path):
@@ -17,14 +17,16 @@ for file_name in os.listdir(folder_path):
                     # Search for 8-digit numbers with digits between 0-9
                     matches = re.findall(r'\b\d{8}\b', comment_text)
                     comments_with_digits.extend(matches)
-            # Make a secondary check to sanitize the array "comments with digits" and remove duplicates as well as invalid numbers
+            # Make a secondary check to sanitize the array "comments with digits" and remove duplicates as well as
+            # invalid numbers
             comments_with_digits = list(set(comments_with_digits))
             # Make sure all the numbers start with 20
             comments_with_digits = [comment for comment in comments_with_digits if comment.startswith("20")]
-            # If there is still more than one number in the array, create a popup that tells the user to check that there is only one student id in the file
+            # If there is still more than one number in the array, create a popup that tells the user to check that
+            # there is only one student id in the file
             if len(comments_with_digits) > 1:
                 print(f"Check file: {file_name} for multiple student IDs")
-                continue # Skip renaming if there are multiple IDs
+                continue  # Skip renaming if there are multiple IDs
         # If there's a single ID, proceed to rename the file
         if comments_with_digits:
             for comment in comments_with_digits:
